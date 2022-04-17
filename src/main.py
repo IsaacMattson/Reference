f8da2f7480032a3ca9ac04b8033c1d2796d89bd9
Isaac <mattsonisaac06@gmail.com>
2022-04-17 16:31:27 -0600

    Made all functions work I think

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from html.parser import HTMLParser
 from datetime import date
 
 
-refURL = "" #this is the url of the page to be Used for reference.
+pageHTML = "" #this is the url of the page to be Used for reference.
 
 #All of the elements for a reference, each will have a assigned function that will asign its value.
 dateAccesed = ""
@@ -28,7 +28,11 @@ def getDateAccessed():
     return todaysDate
 
 def getAuthor():
-    pass
+    input_ = getUserInput("Enter the author, if left empty will default to 'n.a.': ")
+    if input_ == "":
+        input_ = "n.a."
+
+    return input_
 
 def getTitle():
     pass
@@ -36,14 +40,24 @@ def getTitle():
 def getPageURL():
     pass
 
+#Gets the URL using requests and Bs4
+def getHTML():
+    global pageURL
+
+    input_ = getUserInput("Enter the URL: ")
+    pageURL = input_
+
+    page = requests.get(input_)
+    soup = bs4(page.text, 'html.parser')
+
 #This method is for obtaining user input from the cli, 
 #it takes the text to display to user as a parameter and returns the user input.
-#May move to seperate file in future for modularity
+#May move to seperate file in future for modularity(?)
 def getUserInput(_text): 
     input_ = input(_text)
     return input_
 
 def main(): #this is the main method, it will be called when the program is run.
-    pass
+    getHTML()
 main()
 
