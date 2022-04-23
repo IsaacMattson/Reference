1a8b452a7e8520a938537aa362980f02b086eac5
Isaac <mattsonisaac06@gmail.com>
2022-04-22 18:21:08 -0600

    made work

--- src/main.py
+++ src/main.py
@@ -1,84 +1,64 @@
 from sys import getdefaultencoding
+import webbrowser
 import requests
 from bs4 import BeautifulSoup as bs4
 from html.parser import HTMLParser
 from datetime import date
+import website
+
+running = True
+ref = ""
+
+def helpCommand():
+    print("\n\nhelp - displays this message")
+    print("exit - exits the program")
+    print("getweb - gets the data from a webpage")
+    print("getbook - gets the data from a book(not implemented)")
+    print("license - displays the license(not implemented)")
+    print("version - displays the version(not implemented)\n\n")
+def exitCommand():
+    running = False
+    exit()
+def getWebCommand():
+    ref = website.getReferanceWeb()
+    print(ref)
+def getBookCommand():
+    pass
+def licenseCommand():
+    pass
+def versionCommand():
+    pass
 
 
-pageHTML = "" #this is the url of the page to be Used for reference.
-
-output = ""
-
-#All of the elements for a reference, each will have a assigned function that will asign its value.
-dateAccesed = ""
-datePublished = ""
-author = ""
-title = ""
-pageURL = ""
-
-
-
-#these get the data listed in name, and return it.
-def getDatePublished():
-    input_ = getUserInput("Enter the date published(year.month.day), if left empty will default to 'n.d': ")
-    if input_ == "":
-        input_ = "n.d."
-    
-    return input_
-
-def getDateAccessed():
-    todaysDate = date.today()
-    todaysDate = todaysDate.strftime("%d.%m.%Y")
-    return todaysDate
-
-def getAuthor():
-    input_ = getUserInput("Enter the author, if left empty will default to 'n.a.': ")
-    if input_ == "":
-        input_ = "n.a."
-
-    return input_
-
-def getTitle():
-    global pageURL
-    page = requests.get(pageURL)
-    soup = bs4(page.text, 'html.parser')
-    return  soup.title.text
-
-def getPageURL():
-    global pageURL
-
-    input_ = getUserInput("Enter the URL: ")
-    pageURL = input_
-    return input_
-
-#Gets the URL using requests and Bs4
-def getHTML():
-    global pageURL
-    global title
-
-    input_ = getUserInput("Enter the URL: ")
-    pageURL = input_
-
-    page = requests.get(input_)
-    soup = bs4(page.text, 'html.parser')
-    title = soup.title.text
-
-#This method is for obtaining user input from the cli, 
-#it takes the text to display to user as a parameter and returns the user input.
-#May move to seperate file in future for modularity(?)
-def getUserInput(_text): 
-    input_ = input(_text)
-    return input_
 
 def main(): #this is the main method, it will be called when the program is run.
-    author = (getAuthor())
-    datePublished = (getDatePublished())
-    dateAccesed = (getDateAccessed())
-    getPageURL()
-    title = (getTitle())
+     
+    while True:
+        getCommand()
 
-    output = "{}.({}).\x1B[3m{}\x1B[0m.\n   Retrived {} From {}".format(author, datePublished,title, dateAccesed, pageURL)
-    print(output)
+def getCommand():
+    input_ = input("Enter command(help for list of commands): ")
+    if input_ == "help":
+        helpCommand()
+    elif input_ == "exit":
+        exitCommand()
+    elif input_ == "getweb":
+        getWebCommand()
+        
+    elif input_ == "getbook":
+        getBookCommand()
+    elif input_ == "license":
+        licenseCommand()
+    elif input_ == "version":
+        versionCommand()
+    else:
+        print("Invalid command: " + input_ + " (Try " + "\"help\"" + " for a list of commands)")
+
+    
+
+
+        
+
 
 main()
 
--- src/website.py
+++ src/website.py
@@ -1,1 +1,82 @@
 #functions nessasry to getting content from the website
+from sys import getdefaultencoding
+import requests
+from bs4 import BeautifulSoup as bs4
+from html.parser import HTMLParser
+from datetime import date
+
+
+pageHTML = "" #this is the url of the page to be Used for reference.
+
+output = ""
+
+#All of the elements for a reference, each will have a assigned function that will asign its value.
+dateAccesed = ""
+datePublished = ""
+author = ""
+title = ""
+pageURL = ""
+
+
+
+#these get the data listed in name, and return it.
+def getDatePublished():
+    input_ = getUserInput("Enter the date published(year.month.day), if left empty will default to 'n.d': ")
+    if input_ == "":
+        input_ = "n.d."
+    
+    return input_
+
+def getDateAccessed():
+    todaysDate = date.today()
+    todaysDate = todaysDate.strftime("%d.%m.%Y")
+    return todaysDate
+
+def getAuthor():
+    input_ = getUserInput("Enter the author, if left empty will default to 'n.a.': ")
+    if input_ == "":
+        input_ = "n.a."
+
+    return input_
+
+def getTitle():
+    global pageURL
+    page = requests.get(pageURL)
+    soup = bs4(page.text, 'html.parser')
+    return  soup.title.text
+
+def getPageURL():
+    global pageURL
+
+    input_ = getUserInput("Enter the URL: ")
+    pageURL = input_
+    return input_
+
+#Gets the URL using requests and Bs4
+def getHTML():
+    global pageURL
+    global title
+
+    input_ = getUserInput("Enter the URL: ")
+    pageURL = input_
+
+    page = requests.get(input_)
+    soup = bs4(page.text, 'html.parser')
+    title = soup.title.text
+
+#This method is for obtaining user input from the cli, 
+#it takes the text to display to user as a parameter and returns the user input.
+#May move to seperate file in future for modularity(?)
+def getUserInput(_text): 
+    input_ = input(_text)
+    return input_
+
+def getReferanceWeb(): #this is the main method, it will be called when the program is run.
+    author = (getAuthor())
+    datePublished = (getDatePublished())
+    dateAccesed = (getDateAccessed())
+    getPageURL()
+    title = (getTitle())
+
+    output = "{}.({}).\x1B[3m{}\x1B[0m.\n   Retrived {} From {}".format(author, datePublished,title, dateAccesed, pageURL)
+    return output
